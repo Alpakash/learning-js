33921225ee9f37591404c8ffdac8d6e873a6445c
Akash Soedamah <akash.soedamah@gmail.com>
2021-12-27 07:38:18 +0100

    Added some Array methods

--- js/arrays.js
+++ js/arrays.js
@@ -36,4 +36,35 @@ console.log(`The second element from the nested Array is number ${nestedArray[3]
 const restArray = ["first", "second", ...arr];
 console.log(`Add another array as 'the rest' at the end: ${restArray}`);
 
-// ⭐
+// ⭐ Merging two Arrays
+let Array1 = ["Hey", "Hello", "Ola"];
+let Array2 = ["Hoe gaat het?", "How are you?", "Commo estas?"];
+
+const mergedArray = [...Array1, ...Array2];
+console.log(mergedArray);
+
+// I think there is no difference between the .concat method and merging Arrays via ...spread
+Array1 = [1, 2];
+Array2 = [3, 4];
+Array3 = [5, 6];
+
+const merge1 = Array1.concat(Array2, Array3);
+const merge2 = [...Array1, ...Array2, ...Array3];
+
+console.log("Two ways of merging an Array",
+	"1: ", merge1,
+	"2: ", merge2
+	)
+
+// shift (add to beginning) and unshift (remove from beginning) methods
+Array1.shift()
+console.log("shift (remove) index 0 ", Array1);
+
+
+Array2.unshift("Alles goed?")
+console.log("Add to index 0 ", Array2);
+
+// "Slice" a piece out of the Array
+const SlicingTheArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+console.log(SlicingTheArray.slice(4)); // should slice and return index 4 until last one (index 10)
+
